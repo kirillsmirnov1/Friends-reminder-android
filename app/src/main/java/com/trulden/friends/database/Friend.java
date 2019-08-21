4fca94e38084b65944812288b5c83fdc34e684dd
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-21 13:09:27 +0300

    add Friend entity class

---
+++ app/src/main/java/com/trulden/friends/database/Friend.java
@@ -0,0 +1,47 @@
+package com.trulden.friends.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "friend_table")
+public class Friend {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    private String name;
+
+    private String info;
+
+    public Friend(@NonNull String name, String info){
+        this.name = name;
+        this.info = info;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
