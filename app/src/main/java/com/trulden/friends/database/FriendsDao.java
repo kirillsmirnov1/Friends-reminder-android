86c47e5131c83fe95c7e814d6086e81aca84b73e
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-21 13:14:22 +0300

    add FriendsDao class

---
+++ app/src/main/java/com/trulden/friends/database/FriendsDao.java
@@ -0,0 +1,26 @@
+package com.trulden.friends.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface FriendsDao {
+
+    @Insert
+    void insertFriend(Friend friend);
+
+    @Query("SELECT * FROM friend_table")
+    LiveData<List<Friend>> getAllFriends();
+
+    @Delete
+    void deleteFriend(Friend friend);
+
+    @Update
+    void update(Friend friend);
+}
