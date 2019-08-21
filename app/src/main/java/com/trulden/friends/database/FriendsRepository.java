b7af906302b809bb3cc62ebbfef72d0a3ed8ef50
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-21 13:43:36 +0300

    add FriendsRepository class

---
+++ app/src/main/java/com/trulden/friends/database/FriendsRepository.java
@@ -0,0 +1,77 @@
+package com.trulden.friends.database;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class FriendsRepository {
+
+    private FriendsDao mFriendsDao;
+    private LiveData<List<Friend>> mAllFriends;
+
+    FriendsRepository(Application application){
+        mFriendsDao = FriendsDatabase.getDatabase(application).friendsDao();
+        mAllFriends = mFriendsDao.getAllFriends();
+    }
+
+    enum TaskSelector{
+        ADD_FRIEND,
+        UPDATE_FRIEND,
+        REMOVE_FRIEND
+    }
+
+    LiveData<List<Friend>> getAllFriends() { return mAllFriends; }
+
+    public void insertFriend(Friend friend){
+        new FriendAsyncTask(TaskSelector.ADD_FRIEND, mFriendsDao)
+                .execute(friend);
+    }
+
+    public void updateFriend(Friend friend){
+        new FriendAsyncTask(TaskSelector.UPDATE_FRIEND, mFriendsDao)
+                .execute(friend);
+    }
+
+    public void deleteFriend(Friend friend){
+        new FriendAsyncTask(TaskSelector.REMOVE_FRIEND, mFriendsDao)
+                .execute(friend);
+    }
+
+    private static class FriendAsyncTask extends AsyncTask<Friend, Void, Void>{
+
+        private FriendsDao mFriendsDao;
+        private TaskSelector mTaskSelector;
+
+        FriendAsyncTask(TaskSelector taskSelector, FriendsDao friendsDao){
+            mFriendsDao = friendsDao;
+            mTaskSelector = taskSelector;
+        }
+
+        @Override
+        protected Void doInBackground(Friend... friends) {
+
+            switch (mTaskSelector){
+                case ADD_FRIEND:
+                    mFriendsDao.insertFriend(friends[0]);
+                    break;
+
+                case UPDATE_FRIEND:
+                    mFriendsDao.update(friends[0]);
+                    break;
+
+                case REMOVE_FRIEND:
+                    mFriendsDao.deleteFriend(friends[0]);
+                    break;
+
+                default:
+                    // Do nothing
+            }
+
+            return null;
+        }
+    }
+
+}
