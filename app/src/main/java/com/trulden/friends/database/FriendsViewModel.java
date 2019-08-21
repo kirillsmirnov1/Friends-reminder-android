3073366934242e24729b509b46c07a59dcb3201b
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-21 15:21:47 +0300

    add FriendsViewModel class

---
+++ app/src/main/java/com/trulden/friends/database/FriendsViewModel.java
@@ -0,0 +1,31 @@
+package com.trulden.friends.database;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class FriendsViewModel extends AndroidViewModel {
+
+    private FriendsRepository mRepository;
+    private LiveData<List<Friend>> mAllFriends;
+
+    public FriendsViewModel(@NonNull Application application) {
+        super(application);
+
+        mRepository = new FriendsRepository(application);
+        mAllFriends = mRepository.getAllFriends();
+    }
+
+
+    public LiveData<List<Friend>> getAllFriends() {
+        return mAllFriends;
+    }
+
+    public void addFriend(Friend friend){mRepository.insertFriend(friend);}
+    public void updateFriend(Friend friend){mRepository.updateFriend(friend); }
+    public void deleteFriend(Friend friend){mRepository.deleteFriend(friend);}
+}
