87e203e9b11bb3c02f0cd242e06f810804a0e394
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-27 13:48:32 +0300

    Add FriendPageActivity

---
+++ app/src/main/java/com/trulden/friends/activity/FriendPageActivity.java
@@ -0,0 +1,16 @@
+package com.trulden.friends.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.trulden.friends.R;
+
+public class FriendPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_page);
+    }
+}
