509c7e29baa04e1dde2e735ceb936e87dbf831f3
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-27 14:06:28 +0300

    add OnCreateOptionsMenu to FriendPageActivity

--- app/src/main/java/com/trulden/friends/activity/FriendPageActivity.java
+++ app/src/main/java/com/trulden/friends/activity/FriendPageActivity.java
@@ -3,6 +3,7 @@ package com.trulden.friends.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.trulden.friends.R;
 
@@ -12,5 +13,13 @@ public class FriendPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.selection_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
