1c2dde1852a0079ea2d309e10cc05b8af01a8877
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-27 14:11:51 +0300

    fill FriendPageActivity from intent

--- app/src/main/java/com/trulden/friends/activity/FriendPageActivity.java
+++ app/src/main/java/com/trulden/friends/activity/FriendPageActivity.java
@@ -1,18 +1,35 @@
 package com.trulden.friends.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.trulden.friends.R;
+import static com.trulden.friends.util.Util.*;
 
 public class FriendPageActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TextView mPersonInfo;
+
+    private int personId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_page);
+
+        mToolbar = findViewById(R.id.friend_page_toolbar);
+        mPersonInfo = findViewById(R.id.friend_page_info);
+
+        Intent intent = getIntent();
+        personId = intent.getIntExtra(EXTRA_FRIEND_ID, -1);
+        mToolbar.setTitle(intent.getStringExtra(EXTRA_FRIEND_NAME));
+        mPersonInfo.setText(intent.getStringExtra(EXTRA_FRIEND_INFO));
     }
 
     @Override
