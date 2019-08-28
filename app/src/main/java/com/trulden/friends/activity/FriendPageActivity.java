1a1b66e643fa899489c2f9179fc0138d5769d64a
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-28 10:28:59 +0300

    deleting friend from friend page

--- app/src/main/java/com/trulden/friends/activity/FriendPageActivity.java
+++ app/src/main/java/com/trulden/friends/activity/FriendPageActivity.java
@@ -1,21 +1,29 @@
 package com.trulden.friends.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.trulden.friends.R;
+import com.trulden.friends.database.Friend;
+import com.trulden.friends.database.FriendsViewModel;
+
 import static com.trulden.friends.util.Util.*;
 
 public class FriendPageActivity extends AppCompatActivity {
 
     private TextView mPersonInfo;
 
-    private int personId;
+    private Friend friend;
+
+    private FriendsViewModel mFriendsViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +33,16 @@ public class FriendPageActivity extends AppCompatActivity {
         mPersonInfo = findViewById(R.id.friend_page_info);
 
         Intent intent = getIntent();
-        personId = intent.getIntExtra(EXTRA_FRIEND_ID, -1);
-        getSupportActionBar().setTitle(intent.getStringExtra(EXTRA_FRIEND_NAME));
-        mPersonInfo.setText(intent.getStringExtra(EXTRA_FRIEND_INFO));
+
+        friend = new Friend(
+                intent.getIntExtra(EXTRA_FRIEND_ID, -1),
+                intent.getStringExtra(EXTRA_FRIEND_NAME),
+                intent.getStringExtra(EXTRA_FRIEND_INFO));
+
+        getSupportActionBar().setTitle(friend.getName());
+        mPersonInfo.setText(friend.getInfo());
+
+        mFriendsViewModel = ViewModelProviders.of(this).get(FriendsViewModel.class);
     }
 
     @Override
@@ -36,5 +51,25 @@ public class FriendPageActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.selection_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.edit_selection: {
+                // TODO
+                break;
+            }
+
+            case R.id.delete_selection: {
+                mFriendsViewModel.deleteFriend(friend);
+                Toast.makeText(this, friend.getName() + " deleted", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
