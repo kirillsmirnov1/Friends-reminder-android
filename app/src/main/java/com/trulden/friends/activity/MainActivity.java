be9cf187e809db93cdf3c4d82fb048032dfda2a2
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-03 13:30:12 +0300

    rm unused import

--- app/src/main/java/com/trulden/friends/activity/MainActivity.java
+++ app/src/main/java/com/trulden/friends/activity/MainActivity.java
@@ -29,7 +29,6 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Objects;
