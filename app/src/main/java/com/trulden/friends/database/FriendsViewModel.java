8d100d042ed44538b532dbf9c1ebe4f95e49c266
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-04 12:48:50 +0300

    add InteractionTypes to ViewModel

--- app/src/main/java/com/trulden/friends/database/FriendsViewModel.java
+++ app/src/main/java/com/trulden/friends/database/FriendsViewModel.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 import com.trulden.friends.database.entity.Friend;
+import com.trulden.friends.database.entity.InteractionType;
 
 import java.util.List;
 
@@ -14,17 +15,22 @@ public class FriendsViewModel extends AndroidViewModel {
 
     private FriendsRepository mRepository;
     private LiveData<List<Friend>> mAllFriends;
+    private LiveData<List<InteractionType>> mAllInteractionTypes;
 
     public FriendsViewModel(@NonNull Application application) {
         super(application);
 
         mRepository = new FriendsRepository(application);
         mAllFriends = mRepository.getAllFriends();
+        mAllInteractionTypes = mRepository.getAllInteractionTypes();
     }
 
 
     public LiveData<List<Friend>> getAllFriends() {
         return mAllFriends;
+    }
+    public LiveData<List<InteractionType>> getAllInteractionTypes() {
+        return mAllInteractionTypes;
     }
 
     public void addFriend(Friend friend){mRepository.add(friend);}
