29e37636f4110141b99a3cc9e9540841e7e1dcdd
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-04 10:01:50 +0300

    add constants to util

--- app/src/main/java/com/trulden/friends/util/Util.java
+++ app/src/main/java/com/trulden/friends/util/Util.java
@@ -3,6 +3,8 @@ package com.trulden.friends.util;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.trulden.friends.BuildConfig;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -26,6 +28,11 @@ public class Util {
     public static final String EXTRA_FRIEND_INFO = "EXTRA_FRIEND_INFO";
 
     public static final String EXTRA_FRAGMENT_TO_LOAD = "EXTRA_FRAGMENT_TO_LOAD";
+
+    public static final String EXTRA_EXPORT_RESULT = "EXTRA_EXPORT_RESULT";
+
+    public static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
 
     public static void makeToast(Context context, String text){
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
