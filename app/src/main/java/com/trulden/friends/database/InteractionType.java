2635b3984d85841646d51f73a45114ebce3fb244
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-04 11:52:45 +0300

    add InteractionType class

---
+++ app/src/main/java/com/trulden/friends/database/InteractionType.java
@@ -0,0 +1,59 @@
+package com.trulden.friends.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "interaction_type_table")
+public class InteractionType {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    private String interactionTypeName;
+
+    private int frequency;
+
+    public InteractionType(@NonNull String interactionTypeName, int frequency){
+        this.interactionTypeName = interactionTypeName;
+        this.frequency = frequency;
+    }
+
+    @Ignore
+    public InteractionType(int id, @NonNull String interactionTypeName, int frequency){
+        this.id = id;
+        this.interactionTypeName = interactionTypeName;
+        this.frequency = frequency;
+    }
+
+    // -----------------------------------------
+    // Getters and setters
+    // -----------------------------------------
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getInteractionTypeName() {
+        return interactionTypeName;
+    }
+
+    public void setInteractionTypeName(@NonNull String interactionTypeName) {
+        this.interactionTypeName = interactionTypeName;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+}
