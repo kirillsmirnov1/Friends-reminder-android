3b4a2b2307769a248bc2c03a38972397ef7171bd
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-09 11:04:09 +0300

    rm empty line

--- app/src/main/java/com/trulden/friends/activity/LogFragment.java
+++ app/src/main/java/com/trulden/friends/activity/LogFragment.java
@@ -31,7 +31,6 @@ public class LogFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
