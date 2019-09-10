de9329230b518b625f3931ad91f49d97bd5d86b7
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-10 13:29:27 +0300

    fix fields

--- app/src/main/java/com/trulden/friends/activity/dialogs/EditInteractionTypeDialog.java
+++ app/src/main/java/com/trulden/friends/activity/dialogs/EditInteractionTypeDialog.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,34 +13,44 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.trulden.friends.R;
+import com.trulden.friends.activity.EditInteractionActivity;
 
 import static com.trulden.friends.util.Util.*;
 
 public class EditInteractionTypeDialog extends DialogFragment {
+
+    private EditText mName;
+    private EditText mFrequency;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_edit_interaction_type, null);
 
         builder
             .setTitle("Edit interaction type")
-            .setView(inflater.inflate(R.layout.dialog_edit_interaction_type, null))
+            .setView(dialogView)
             .setPositiveButton("Save", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    // TODO save to database
-                    makeToast(getActivity(), "Should be created");
+                    ((EditInteractionActivity)getActivity()) // FIXME mName is null
+                            .createInteractionType(mName.getText().toString(), Integer.parseInt(mFrequency.getText().toString()));
                 }
             })
             .setNegativeButton("Discard", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    // TODO nothing, I guess
                 }
             });
 
-        return builder.create();
+        Dialog dialog = builder.create();
+
+        mName = dialogView.findViewById(R.id.edit_interaction_type_name);
+        mFrequency = dialogView.findViewById(R.id.edit_interaction_type_frequency);
+
+        return dialog;
     }
 }
