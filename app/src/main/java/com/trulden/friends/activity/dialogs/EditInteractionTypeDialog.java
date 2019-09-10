96e1425a9af0631c320732b0a93b2ca727b2da57
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-10 13:02:44 +0300

    inflate dialog with custom view

--- app/src/main/java/com/trulden/friends/activity/dialogs/EditInteractionTypeDialog.java
+++ app/src/main/java/com/trulden/friends/activity/dialogs/EditInteractionTypeDialog.java
@@ -3,11 +3,14 @@ package com.trulden.friends.activity.dialogs;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+
+import com.trulden.friends.R;
 
 import static com.trulden.friends.util.Util.*;
 
@@ -17,8 +20,11 @@ public class EditInteractionTypeDialog extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
         builder
             .setTitle("Edit interaction type")
+            .setView(inflater.inflate(R.layout.dialog_edit_interaction_type, null))
             .setPositiveButton("Save", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
