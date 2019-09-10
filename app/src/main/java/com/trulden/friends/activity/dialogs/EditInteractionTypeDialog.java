ac5da1585ea7dc5820aaffb084e1328457f9d407
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-10 12:58:54 +0300

    fill dialog

--- app/src/main/java/com/trulden/friends/activity/dialogs/EditInteractionTypeDialog.java
+++ app/src/main/java/com/trulden/friends/activity/dialogs/EditInteractionTypeDialog.java
@@ -1,7 +1,38 @@
 package com.trulden.friends.activity.dialogs;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
-public class EditInteractionTypeDialog extends DialogFragment {
+import static com.trulden.friends.util.Util.*;
 
+public class EditInteractionTypeDialog extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder
+            .setTitle("Edit interaction type")
+            .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // TODO save to database
+                    makeToast(getActivity(), "Should be created");
+                }
+            })
+            .setNegativeButton("Discard", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // TODO nothing, I guess
+                }
+            });
+
+        return builder.create();
+    }
 }
