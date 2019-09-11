4312af2229694dc2f9cc2a0dc98a72db52454ccd
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-11 16:05:56 +0300

    and some more

--- app/src/main/java/com/trulden/friends/activity/InteractionTypesActivity.java
+++ app/src/main/java/com/trulden/friends/activity/InteractionTypesActivity.java
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.trulden.friends.R;
+import com.trulden.friends.adapter.InteractionTypeAdapter;
 import com.trulden.friends.database.FriendsViewModel;
 
 public class InteractionTypesActivity extends AppCompatActivity {
 
     private FriendsViewModel mFriendsViewModel;
+
+    private InteractionTypeAdapter mInteractionTypeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
