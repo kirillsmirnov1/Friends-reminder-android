87f169b8f7a9dc978ebc43c105201c1aa250b7ab
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-12 09:31:45 +0300

    connect to database

--- app/src/main/java/com/trulden/friends/activity/LastInteractionsFragment.java
+++ app/src/main/java/com/trulden/friends/activity/LastInteractionsFragment.java
@@ -2,23 +2,34 @@ package com.trulden.friends.activity;
 
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
+
 import com.google.android.material.tabs.TabLayout;
 import com.trulden.friends.R;
 import com.trulden.friends.adapter.PagerAdapter;
 import com.trulden.friends.adapter.TabCounterView;
+import com.trulden.friends.database.FriendsViewModel;
+import com.trulden.friends.database.entity.InteractionType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LastInteractionsFragment extends Fragment {
+
+    private FriendsViewModel friendsViewModel;
+    private List<InteractionType> types = new ArrayList<>();
 
     private TabLayout mTabLayout;
 
@@ -37,6 +48,15 @@ public class LastInteractionsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        friendsViewModel = ViewModelProviders.of(getActivity()).get(FriendsViewModel.class);
+
+        friendsViewModel.getAllInteractionTypes().observe(this, new Observer<List<InteractionType>>() {
+            @Override
+            public void onChanged(List<InteractionType> interactionTypes) {
+                types = interactionTypes;
+            }
+        });
 
         initTabsAndPageViewer(view);
     }
