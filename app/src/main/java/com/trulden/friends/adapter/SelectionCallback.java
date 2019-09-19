3aab774568190a6132dac53adc050cf1f6157923
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-19 09:26:31 +0300

    class SelectionCallback implements ActionMode.Callback

---
+++ app/src/main/java/com/trulden/friends/adapter/SelectionCallback.java
@@ -0,0 +1,62 @@
+package com.trulden.friends.adapter;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.view.ActionMode;
+
+import com.trulden.friends.R;
+import com.trulden.friends.activity.interfaces.ActivityWithSelection;
+
+/**
+ * Handles choose of selection options
+ * */
+public class SelectionCallback implements ActionMode.Callback {
+
+    private ActivityWithSelection mActivity;
+    private CustomRVAdapter mAdapter;
+
+    public SelectionCallback(ActivityWithSelection activity, CustomRVAdapter adapter){
+        super();
+
+        mActivity = activity;
+        mAdapter = adapter;
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        mode.getMenuInflater().inflate(R.menu.selection_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.delete_selection: {
+                mActivity.deleteSelection();
+                mode.finish();
+                return true;
+            }
+            case R.id.edit_selection: {
+                mActivity.editSelection();
+                mode.finish();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+        mAdapter.clearSelections();
+        mActivity.nullActionMode();
+    }
+}
