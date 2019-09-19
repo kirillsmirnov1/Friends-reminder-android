c2c0631d06cfd4a8e4ad7a3cb143ca64b4a62697
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-19 10:08:41 +0300

    store type object in LastInteraction

--- app/src/main/java/com/trulden/friends/database/entity/LastInteraction.java
+++ app/src/main/java/com/trulden/friends/database/entity/LastInteraction.java
@@ -1,14 +1,20 @@
 package com.trulden.friends.database.entity;
 
 import androidx.annotation.NonNull;
+import androidx.room.Relation;
+
+import java.util.List;
 
 public class LastInteraction extends AbstractEntity {
 
-    private String type;
+    private String type; // TODO store id
 
     private long date;
 
     private String friend;
+
+    @Relation(parentColumn = "type", entityColumn = "interactionTypeName", entity = InteractionType.class)
+    private List<InteractionType> interactionTypes;
 
     public LastInteraction(@NonNull String type, long date, String friend){
         this.type = type;
@@ -42,5 +48,17 @@ public class LastInteraction extends AbstractEntity {
 
     public void setFriend(String friend) {
         this.friend = friend;
+    }
+
+    public InteractionType getInteractionType() {
+        return interactionTypes.get(0);
+    }
+
+    public void setInteractionTypes(List<InteractionType> interactionTypes) {
+        this.interactionTypes = interactionTypes;
+    }
+
+    public List<InteractionType> getInteractionTypes() {
+        return interactionTypes;
     }
 }
