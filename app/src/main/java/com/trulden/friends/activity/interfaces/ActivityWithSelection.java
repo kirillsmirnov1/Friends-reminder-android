d61fb1f3893352931dbb4ccf23cb72fb439059ed
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-20 13:55:24 +0300

    comment ActivityWithSelection

--- app/src/main/java/com/trulden/friends/activity/interfaces/ActivityWithSelection.java
+++ app/src/main/java/com/trulden/friends/activity/interfaces/ActivityWithSelection.java
@@ -1,7 +1,22 @@
 package com.trulden.friends.activity.interfaces;
 
+/**
+ * Interface with edit/delete selection and nullifyActionMode methods
+ */
 public interface ActivityWithSelection {
+    /**
+     * Open editing activity for selected item.
+     * Must work only when one item selected
+     */
     void editSelection();
+
+    /**
+     * Delete selected items
+     */
     void deleteSelection();
+
+    /**
+     * Set actionMode = null
+     */
     void nullifyActionMode();
 }
