c26902c944f808c38a7d6555a10fb6a669f86f8b
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-20 13:58:18 +0300

    comment EditInteractionType

--- app/src/main/java/com/trulden/friends/activity/interfaces/EditInteractionType.java
+++ app/src/main/java/com/trulden/friends/activity/interfaces/EditInteractionType.java
@@ -2,7 +2,20 @@ package com.trulden.friends.activity.interfaces;
 
 import com.trulden.friends.database.entity.InteractionType;
 
+/**
+ * Interface for classes which can create or edit InteractionType objects
+ */
 public interface EditInteractionType {
+    /**
+     * Checks if type already exists
+     * @param typeName name of type to be checked
+     * @return true, if type exists, false otherwise
+     */
     boolean typeExists(String typeName);
+
+    /**
+     * Save type to database
+     * @param interactionType type to be saved
+     */
     void saveType(InteractionType interactionType);
 }
