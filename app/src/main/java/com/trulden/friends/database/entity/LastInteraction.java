b2e5737971aeb273353b097159673a8ee77427a1
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-20 11:05:56 +0300

    comment LastInteraction

--- app/src/main/java/com/trulden/friends/database/entity/LastInteraction.java
+++ app/src/main/java/com/trulden/friends/database/entity/LastInteraction.java
@@ -6,7 +6,13 @@ import java.util.List;
 
 import static com.trulden.friends.util.Util.daysPassed;
 
+/**
+ * Shows how much time passed since interaction of some type with some friend.
+ * Not stored in database.
+ */
 public class LastInteraction implements Entity {
+
+    // TODO store in database
 
     private int typeId;
 
@@ -17,6 +23,12 @@ public class LastInteraction implements Entity {
     @Relation(parentColumn = "typeId", entityColumn = "id", entity = InteractionType.class)
     private List<InteractionType> interactionTypes;
 
+    /**
+     * Constructor for LI entry. Must be used only by DAO
+     * @param typeId type of Interaction
+     * @param date Unix epoch time of interaction
+     * @param friend name of friend
+     */
     public LastInteraction(int typeId, long date, String friend){
         this.typeId = typeId;
         this.date = date;
@@ -59,6 +71,8 @@ public class LastInteraction implements Entity {
     public void setInteractionTypes(List<InteractionType> interactionTypes) {
         this.interactionTypes = interactionTypes;
     }
+
+    // FIXME those methods are required by room, but I couldn't find a way to suppress warnings for them
 
     public List<InteractionType> getInteractionTypes() {
         return interactionTypes;
