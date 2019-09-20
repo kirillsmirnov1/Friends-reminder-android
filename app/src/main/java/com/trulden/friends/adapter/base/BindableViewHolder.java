2711bcc9d0471d123e0dedc52891a7aed53abb91
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-20 11:43:56 +0300

    Comment BindableViewHolder

--- app/src/main/java/com/trulden/friends/adapter/base/BindableViewHolder.java
+++ app/src/main/java/com/trulden/friends/adapter/base/BindableViewHolder.java
@@ -2,9 +2,14 @@ package com.trulden.friends.adapter.base;
 
 import com.trulden.friends.database.entity.Entity;
 /**
- * interface with bindTo method
+ * Interface with bindTo method
  * @param <T_E> Entity, passed to binded view
  * */
 public interface BindableViewHolder<T_E extends Entity> {
+    /**
+     * Binds an entity to a position
+     * @param entity object to be binded
+     * @param position position to which object will be binded
+     */
     void bindTo(final T_E entity, final int position);
 }
