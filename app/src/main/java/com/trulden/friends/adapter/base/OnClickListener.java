80e1ed4168e6879110abacc9930eeeb8b5b7c809
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-20 12:07:32 +0300

    comment OnClickListener

--- app/src/main/java/com/trulden/friends/adapter/base/OnClickListener.java
+++ app/src/main/java/com/trulden/friends/adapter/base/OnClickListener.java
@@ -2,6 +2,10 @@ package com.trulden.friends.adapter.base;
 
 import android.view.View;
 
+/**
+ * Used for creating anonymous listeners in RecyclerView adapters
+ * @param <T> class of object used in RV
+ */
 public interface OnClickListener<T> {
     void onItemClick(View view, T obj, int pos);
     void onItemLongClick(View view, T obj, int pos);
