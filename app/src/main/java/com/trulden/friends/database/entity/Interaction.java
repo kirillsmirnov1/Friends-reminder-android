04dd4e7b530c4323fbc1032d2d5ff833ada41acc
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-23 11:38:19 +0300

    @see → @link

--- app/src/main/java/com/trulden/friends/database/entity/Interaction.java
+++ app/src/main/java/com/trulden/friends/database/entity/Interaction.java
@@ -56,15 +56,12 @@ public class Interaction implements Entity {
     /**
      * Constructor for updating existing Interaction
      * @param id id of Interaction in database
-     * @param interactionTypeId id of type of interaction
+     * @param interactionTypeId id of {@link InteractionType}
      * @param date date of interaction in Unix epoch time
      * @param comment plain text notes about interaction
      * @param friendNames plain text friend names. Should be divided by comma.
      *                    Should be used only for ease of demonstration.
-     *                    Access to real friends object should be handled through BindFriendInteraction.
-     *
-     * @see InteractionType InteractionType
-     * @see BindFriendInteraction BindFriendInteraction
+     *                    Access to real friends object should be handled through {@link BindFriendInteraction}.
      */
     @Ignore
     public Interaction(long id, long interactionTypeId, long date, String comment, String friendNames) {
