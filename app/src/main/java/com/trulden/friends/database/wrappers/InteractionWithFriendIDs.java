2eb38ead151c191daa4e4daf2faf8f6b9415701c
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-24 11:27:36 +0300

    add InteractionWithFriendIDs class

---
+++ app/src/main/java/com/trulden/friends/database/wrappers/InteractionWithFriendIDs.java
@@ -0,0 +1,22 @@
+package com.trulden.friends.database.wrappers;
+
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+import com.trulden.friends.database.entity.Friend;
+import com.trulden.friends.database.entity.Interaction;
+
+import java.util.List;
+
+/**
+ * Stores an Interaction and IDs of friends, related to that interaction
+ */
+public class InteractionWithFriendIDs {
+    @Embedded
+    public Interaction interaction;
+
+    @Relation(
+        parentColumn = "id", entityColumn = "id",
+        entity = Friend.class, projection = {"id"})
+    public List<Integer> friendIDs;
+}
