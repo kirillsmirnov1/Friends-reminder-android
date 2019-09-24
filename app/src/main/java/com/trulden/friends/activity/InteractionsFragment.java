abfc05b0cc360b7bfc60089a5ab8b3e86edf800f
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-24 14:38:00 +0300

    refactor fragment_log → fragment_interactions

--- app/src/main/java/com/trulden/friends/activity/InteractionsFragment.java
+++ app/src/main/java/com/trulden/friends/activity/InteractionsFragment.java
@@ -63,7 +63,7 @@ public class InteractionsFragment extends Fragment implements ActivityWithSelect
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log, container, false);
+        return inflater.inflate(R.layout.fragment_interactions, container, false);
     }
 
     @Override
