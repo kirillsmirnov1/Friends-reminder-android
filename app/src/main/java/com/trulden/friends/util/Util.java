28b6bdb19e4bea8f26c414af43296c0214bb7e9a
kirillsmirnov1 <thegaldren@gmail.com>
2019-09-25 12:44:23 +0300

    add daysBetween() calculating calendar days between two calendar instances

--- app/src/main/java/com/trulden/friends/util/Util.java
+++ app/src/main/java/com/trulden/friends/util/Util.java
@@ -114,4 +114,33 @@ public abstract class Util {
         long timePassed = Calendar.getInstance().getTimeInMillis() - interaction.getDate();
         return (int) ( timePassed / MILLISECONDS_IN_DAYS );
     }
+
+    private static int calendarDaysBetween(Calendar day1, Calendar day2){
+        // Taken from here https://stackoverflow.com/a/28865648/11845909
+
+        Calendar dayOne = (Calendar) day1.clone(),
+                 dayTwo = (Calendar) day2.clone();
+
+        if (dayOne.get(Calendar.YEAR) == dayTwo.get(Calendar.YEAR)) {
+            return Math.abs(dayOne.get(Calendar.DAY_OF_YEAR) - dayTwo.get(Calendar.DAY_OF_YEAR));
+        } else {
+            if (dayTwo.get(Calendar.YEAR) > dayOne.get(Calendar.YEAR)) {
+                //swap them
+                Calendar temp = dayOne;
+                dayOne = dayTwo;
+                dayTwo = temp;
+            }
+            int extraDays = 0;
+
+            int dayOneOriginalYearDays = dayOne.get(Calendar.DAY_OF_YEAR);
+
+            while (dayOne.get(Calendar.YEAR) > dayTwo.get(Calendar.YEAR)) {
+                dayOne.add(Calendar.YEAR, -1);
+                // getActualMaximum() important for leap years
+                extraDays += dayOne.getActualMaximum(Calendar.DAY_OF_YEAR);
+            }
+
+            return extraDays - dayTwo.get(Calendar.DAY_OF_YEAR) + dayOneOriginalYearDays ;
+        }
+    }
 }
