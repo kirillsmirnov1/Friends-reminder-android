55ce27f48cade489516c706d2619215af8a28299
kirillsmirnov1 <thegaldren@gmail.com>
2019-10-01 13:05:10 +0300

    fix tab tests so they work correctly when passing all of the tests

--- app/src/androidTest/java/com/trulden/friends/activity/TabTest.java
+++ app/src/androidTest/java/com/trulden/friends/activity/TabTest.java
@@ -27,21 +27,100 @@ import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
-public class TabSwitchTest {
+public class TabTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    private void openLastInteractionFragment(){
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.bottom_last_interactions), withContentDescription("Last Interactions"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_navigation),
+                                        0),
+                                1),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        try {
+            Thread.sleep(250);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void tabPersistenceTest() {
+
+        DatabaseTestingHandler.initAndFillDatabase(
+                (FragmentActivity) TestUtil.getActivityInstance());
+
+        openLastInteractionFragment();
+
+        ViewInteraction tabView = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.last_interactions_tab_layout),
+                                0),
+                        1),
+                        isDisplayed()));
+        tabView.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.tab_label), withText("Texting"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Texting")));
+
+        ViewInteraction bottomNavigationItemView2 = onView(
+                allOf(withId(R.id.bottom_friends), withContentDescription("Friends"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_navigation),
+                                        0),
+                                2),
+                        isDisplayed()));
+        bottomNavigationItemView2.perform(click());
+
+        ViewInteraction bottomNavigationItemView3 = onView(
+                allOf(withId(R.id.bottom_last_interactions), withContentDescription("Last Interactions"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_navigation),
+                                        0),
+                                1),
+                        isDisplayed()));
+        bottomNavigationItemView3.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.tab_label), withText("Texting"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("Texting")));
+    }
 
     @Test
     public void tabClickSwitchTest() {
 
         DatabaseTestingHandler.initAndFillDatabase(
                 (FragmentActivity) TestUtil.getActivityInstance());
+
+        openLastInteractionFragment();
 
         ViewInteraction textView = onView(
                 allOf(withId(R.id.last_interaction_name), withText("Caleb"),
@@ -89,6 +168,8 @@ public class TabSwitchTest {
 
         DatabaseTestingHandler.initAndFillDatabase(
                 (FragmentActivity) TestUtil.getActivityInstance());
+
+        openLastInteractionFragment();
 
         ViewInteraction textView = onView(
                 allOf(withId(R.id.last_interaction_name), withText("Caleb"),
