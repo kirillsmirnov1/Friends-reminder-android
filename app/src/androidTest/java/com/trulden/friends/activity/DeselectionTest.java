6ee152e59110beedf6dfe89531aa3f954725831b
kirillsmirnov1 <thegaldren@gmail.com>
2019-10-01 13:34:53 +0300

    add delete-deselection tests

--- app/src/androidTest/java/com/trulden/friends/activity/DeselectionTest.java
+++ app/src/androidTest/java/com/trulden/friends/activity/DeselectionTest.java
@@ -41,7 +41,7 @@ public class DeselectionTest {
 
     @Test
     public void manualDeselectionLogTest() {
-        DatabaseTestingHandler.initAndFillDatabase(
+        DatabaseTestingHandler.initAndFillDatabase( // FIXME why not @BeforeEveryone?
                 (FragmentActivity) TestUtil.getActivityInstance());
 
         ViewInteraction bottomNavigationItemView = onView(
@@ -664,6 +664,216 @@ public class DeselectionTest {
 
         ViewInteraction textView2 = onView(
                 allOf(withText("1"),
+                        isDisplayed()));
+        textView2.check(matches(withText("1")));
+    }
+
+    @Test
+    public void logDeleteDeselectionTest() {
+        DatabaseTestingHandler.initAndFillDatabase(
+                (FragmentActivity) TestUtil.getActivityInstance());
+
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.bottom_interactions), withContentDescription("Log"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_navigation),
+                                        0),
+                                0),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction constraintLayout = onView(
+                allOf(withId(R.id.interaction_entry_layout),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.interactions_recyclerview),
+                                        0),
+                                0),
+                        isDisplayed()));
+        constraintLayout.perform(longClick());
+
+        ViewInteraction constraintLayout2 = onView(
+                allOf(withId(R.id.interaction_entry_layout),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.interactions_recyclerview),
+                                        1),
+                                0),
+                        isDisplayed()));
+        constraintLayout2.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withText("2"),
+                        isDisplayed()));
+        textView.check(matches(withText("2")));
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.delete_selection), withContentDescription("Delete selection"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_mode_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction constraintLayout3 = onView(
+                allOf(withId(R.id.interaction_entry_layout),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.interactions_recyclerview),
+                                        0),
+                                0),
+                        isDisplayed()));
+        constraintLayout3.perform(longClick());
+
+        ViewInteraction textView2 = onView(
+                allOf(withText("1"),
+                        isDisplayed()));
+        textView2.check(matches(withText("1")));
+    }
+
+    @Test
+    public void friendsDeleteDeselectionTest() {
+        DatabaseTestingHandler.initAndFillDatabase(
+                (FragmentActivity) TestUtil.getActivityInstance());
+
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.bottom_friends),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction constraintLayout = onView(
+                allOf(withId(R.id.friend_entry_layout),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.friends_recyclerView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        constraintLayout.perform(longClick());
+
+        ViewInteraction constraintLayout2 = onView(
+                allOf(withId(R.id.friend_entry_layout),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.friends_recyclerView),
+                                        1),
+                                0),
+                        isDisplayed()));
+        constraintLayout2.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withText("2"),
+                        isDisplayed()));
+        textView.check(matches(withText("2")));
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.delete_selection), withContentDescription("Delete selection"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_mode_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction constraintLayout3 = onView(
+                allOf(withId(R.id.friend_entry_layout),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.friends_recyclerView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        constraintLayout3.perform(longClick());
+
+        ViewInteraction textView2 = onView(
+                allOf(withText("1"),
+                        isDisplayed()));
+        textView2.check(matches(withText("1")));
+    }
+
+    @Test
+    public void typesDeleteDeselectionTest() {
+        DatabaseTestingHandler.initAndFillDatabase(
+                (FragmentActivity) TestUtil.getActivityInstance());
+
+        ViewInteraction overflowMenuButton2 = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.toolbar),
+                                1),
+                        0),
+                        isDisplayed()));
+        overflowMenuButton2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(250);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withId(R.id.title), withText("Interaction types"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction relativeLayout = onView(
+                allOf(withId(R.id.interaction_type_entry_layout),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.interaction_type_recyclerview),
+                                        0),
+                                0),
+                        isDisplayed()));
+        relativeLayout.perform(longClick());
+
+        ViewInteraction textView = onView(
+                allOf(withText("1"),
+                        isDisplayed()));
+        textView.check(matches(withText("1")));
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.delete_selection), withContentDescription("Delete selection"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_context_bar),
+                                        1),
+                                1),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction relativeLayout2 = onView(
+                allOf(withId(R.id.interaction_type_entry_layout),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.interaction_type_recyclerview),
+                                        0),
+                                0),
+                        isDisplayed()));
+        relativeLayout2.perform(longClick());
+
+        ViewInteraction textView2 = onView(
+                allOf( withText("1"),
                         isDisplayed()));
         textView2.check(matches(withText("1")));
     }
