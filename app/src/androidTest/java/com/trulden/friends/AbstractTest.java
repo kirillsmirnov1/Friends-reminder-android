d8f4606efb4ee1d4b0dde4c8cf055d77e465aa36
kirillsmirnov1 <thegaldren@gmail.com>
2019-10-03 19:08:55 +0300

    add Matcher<Root> isToast()

--- app/src/androidTest/java/com/trulden/friends/AbstractTest.java
+++ app/src/androidTest/java/com/trulden/friends/AbstractTest.java
@@ -1,10 +1,13 @@
 package com.trulden.friends;
 
+import android.os.IBinder;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.view.WindowManager;
 
 import androidx.fragment.app.FragmentActivity;
+import androidx.test.espresso.Root;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.rule.ActivityTestRule;
 
@@ -146,6 +149,31 @@ public abstract class AbstractTest {
             @Override
             public void describeTo(final Description description) {
                 description.appendText("should return first matching item");
+            }
+        };
+    }
+
+    private static Matcher<Root> isToast() {
+        return new TypeSafeMatcher<Root>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("is toast");
+            }
+
+            @Override
+            public boolean matchesSafely(Root root) {
+                int type = root.getWindowLayoutParams().get().type;
+                if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
+                    IBinder windowToken = root.getDecorView().getWindowToken();
+                    IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                    if (windowToken == appToken) {
+                        // windowToken == appToken means this window isn't contained by any other windows.
+                        // if it was a window for an activity, it would have TYPE_BASE_APPLICATION.
+                        return true;
+                    }
+                }
+                return false;
             }
         };
     }
