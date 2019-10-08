5454397fb0f1d6152e6f2b13f002397e7841aa20
kirillsmirnov1 <thegaldren@gmail.com>
2019-10-08 16:14:26 +0300

    fix LI im unit tests

--- app/src/test/java/com/trulden/friends/database/wrappers/LastInteractionTest.java
+++ app/src/test/java/com/trulden/friends/database/wrappers/LastInteractionTest.java
@@ -18,13 +18,16 @@ public class LastInteractionTest {
         Calendar weekAgo = Calendar.getInstance();
         weekAgo.add(Calendar.DATE, -7);
 
-        LastInteraction lastInteraction = new LastInteraction(0, weekAgo.getTimeInMillis(), null);
+        LastInteraction lastInteraction = new LastInteraction(0, 0, 0, weekAgo.getTimeInMillis(), 0);
+        LastInteractionWrapper lastInteractionWrapper = new LastInteractionWrapper();
+        lastInteractionWrapper.setLastInteraction(lastInteraction);
+
         InteractionType type = new InteractionType("8 days", 8);
         ArrayList<InteractionType> typeArrayList = new ArrayList<>();
         typeArrayList.add(type);
 
-        lastInteraction.setInteractionTypes(typeArrayList);
+        lastInteractionWrapper.setTypes(typeArrayList);
 
-        assertFalse(lastInteraction.itsTime());
+        assertFalse(lastInteractionWrapper.itsTime());
     }
 }
