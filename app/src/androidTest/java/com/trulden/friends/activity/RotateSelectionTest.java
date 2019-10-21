94ebd564f473335736609d36f1fe62a99d604521
kirillsmirnov1 <thegaldren@gmail.com>
2019-10-21 11:55:01 +0300

    add rotateInteractionsTest()

--- app/src/androidTest/java/com/trulden/friends/activity/RotateSelectionTest.java
+++ app/src/androidTest/java/com/trulden/friends/activity/RotateSelectionTest.java
@@ -1,6 +1,42 @@
 package com.trulden.friends.activity;
 
+import android.content.pm.ActivityInfo;
+
 import com.trulden.friends.AbstractTest;
 
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
 public class RotateSelectionTest extends AbstractTest {
+
+    @Test
+    public void rotateInteractionsTest(){
+        openLog();
+
+        onView(withText("A + B")).perform(longClick());
+
+        onView(withText("1")).check(matches(isDisplayed()));
+
+        mActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        sleep(500);
+
+        onView(withText("1")).check(matches(isDisplayed()));
+
+        mActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        sleep(500);
+
+        onView(withText("1")).check(matches(isDisplayed()));
+
+        onView(withText("A + B")).perform(click());
+
+        sleep(250);
+
+        onView(withText("1")).check(doesNotExist());
+    }
 }
