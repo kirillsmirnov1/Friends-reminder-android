1fb6c801bc1a2fd4922f3a441029571405a3d891
kirillsmirnov1 <thegaldren@gmail.com>
2019-10-21 12:06:44 +0300

    add rotateLITest()

--- app/src/androidTest/java/com/trulden/friends/activity/RotateSelectionTest.java
+++ app/src/androidTest/java/com/trulden/friends/activity/RotateSelectionTest.java
@@ -12,12 +12,21 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class RotateSelectionTest extends AbstractTest {
 
     @Test
     public void rotateInteractionsTest(){
         rotateTest("Log", withText("1"), "A + B");
+    }
+
+    @Test
+    public void rotateLITest(){
+
+        Matcher<View> m = allOf(withText("1"), not(hasSibling(withText("Meeting"))));
+
+        rotateTest("LI", m, "Caleb");
     }
 
     private void rotateTest(String activity, Matcher<View> matcher, String entryText){
