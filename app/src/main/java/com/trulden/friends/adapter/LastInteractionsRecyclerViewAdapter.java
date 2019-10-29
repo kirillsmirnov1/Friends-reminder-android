31b8be576d9bef8c8df156514cd0ab1bc70c2a11
kirillsmirnov1 <thegaldren@gmail.com>
2019-10-29 10:57:22 +0300

    show freq on right side

--- app/src/main/java/com/trulden/friends/adapter/LastInteractionsRecyclerViewAdapter.java
+++ app/src/main/java/com/trulden/friends/adapter/LastInteractionsRecyclerViewAdapter.java
@@ -42,10 +42,10 @@ public class LastInteractionsRecyclerViewAdapter extends CustomRVAdapter<LastInt
             extends RecyclerView.ViewHolder
             implements BindableViewHolder<LastInteractionWrapper> {
 
-        private TextView mName;
-        private TextView mTime;
+        private TextView  mName;
+        private TextView  mTime;
         private ImageView mHiddenIcon;
-
+        private TextView  mFrequency;
         private RelativeLayout mLayout;
 
         ViewHolder(@NonNull View itemView) {
@@ -54,6 +54,7 @@ public class LastInteractionsRecyclerViewAdapter extends CustomRVAdapter<LastInt
             mName = itemView.findViewById(R.id.eli_friend_name);
             mTime = itemView.findViewById(R.id.eli_time_passed);
             mHiddenIcon = itemView.findViewById(R.id.eli_hidden_icon);
+            mFrequency = itemView.findViewById(R.id.eli_frequency);
 
             mLayout = itemView.findViewById(R.id.eli_layout);
         }
@@ -73,6 +74,8 @@ public class LastInteractionsRecyclerViewAdapter extends CustomRVAdapter<LastInt
             String dateString = daysPassed + mContext.getString(R.string.days_ago);
 
             mTime.setText(dateString);
+
+            mFrequency.setText("раз в " + interaction.getType().getFrequency() + " д.");
 
             // Grey out LI for which time have not yet come
             if(!interaction.itsTime()) {
