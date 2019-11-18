8edb5bc6a7ed0dd842144a70076e9e2e97d20262
kirillsmirnov1 <thegaldren@gmail.com>
2019-11-18 10:18:08 +0300

    merge selection menus

--- app/src/main/java/com/trulden/friends/activity/InteractionsFragment.java
+++ app/src/main/java/com/trulden/friends/activity/InteractionsFragment.java
@@ -155,9 +155,9 @@ public class InteractionsFragment extends Fragment implements EditAndDeleteSelec
             mActionMode.invalidate();
 
             if(count == 1){
-                mActionMode.getMenu().findItem(R.id.msed_edit).setVisible(true);
+                mActionMode.getMenu().findItem(R.id.ms_edit).setVisible(true);
             } else {
-                mActionMode.getMenu().findItem(R.id.msed_edit).setVisible(false);
+                mActionMode.getMenu().findItem(R.id.ms_edit).setVisible(false);
             }
 
         }
