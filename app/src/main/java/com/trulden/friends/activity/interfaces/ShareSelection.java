1188ef48847a150375fd12587862e7a8b37adb5f
kirillsmirnov1 <thegaldren@gmail.com>
2019-11-18 10:08:50 +0300

    add ShareSelection interface

---
+++ app/src/main/java/com/trulden/friends/activity/interfaces/ShareSelection.java
@@ -0,0 +1,11 @@
+package com.trulden.friends.activity.interfaces;
+
+/**
+ * Shares selection as plain text
+ */
+public interface ShareSelection extends BasicSelection {
+    /**
+     * Share selected items as plain text
+     */
+    void shareSelection();
+}
