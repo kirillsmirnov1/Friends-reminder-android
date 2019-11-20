28d87a6afb702a40455fead808bf0bcb6e0d5d4c
kirillsmirnov1 <thegaldren@gmail.com>
2019-11-20 16:37:13 +0300

    wait for device running

--- make_screenshots.sh
+++ make_screenshots.sh
@@ -1,6 +1,6 @@
-/home/kirill/Android/Sdk/emulator/emulator -avd Nexus_4_API_21 & disown;
+/home/kirill/Android/Sdk/emulator/emulator -avd Nexus_4_API_21 & adb wait-for-device;
 
-echo 'emulator started'
+echo 'emulator is up and running'
 
 rm -r fastlane/metadata/
 bundle exec fastlane capture_screen
