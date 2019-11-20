22a9e719337390cb7c10dec8f9a03ca33f6311af
kirillsmirnov1 <thegaldren@gmail.com>
2019-11-20 18:53:20 +0300

    set emulator date

--- make_screenshots.sh
+++ make_screenshots.sh
@@ -1,5 +1,7 @@
 /home/kirill/Android/Sdk/emulator/emulator -avd Nexus_4_API_21 & adb wait-for-device;
 
+adb shell date -s 20191113.123400
+
 echo 'emulator is up and running'
 
 rm -r fastlane/metadata/
