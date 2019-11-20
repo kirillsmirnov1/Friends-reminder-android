a7635defd66ba87b60221f3991fac5b87e11da79
kirillsmirnov1 <thegaldren@gmail.com>
2019-11-20 19:06:51 +0300

    guarantee day mode and show hidden LI

--- app/src/androidTest/java/com/trulden/friends/screenshot/ScreenshotTest.java
+++ app/src/androidTest/java/com/trulden/friends/screenshot/ScreenshotTest.java
@@ -2,7 +2,11 @@ package com.trulden.friends.screenshot;
 
 import android.app.Activity;
 import android.net.Uri;
+import android.view.View;
+import android.widget.CheckBox;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.rule.ActivityTestRule;
 
 import com.trulden.friends.R;
@@ -10,6 +14,9 @@ import com.trulden.friends.TestUtil;
 import com.trulden.friends.activity.MainActivity;
 import com.trulden.friends.async.ImportDatabaseAsyncTask;
 
+import junit.framework.AssertionFailedError;
+
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -22,11 +29,14 @@ import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
 import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static com.trulden.friends.database.FriendsDatabase.getDatabase;
 import static com.trulden.friends.util.Util.wipeDatabaseFiles;
+import static org.hamcrest.Matchers.*;
 
 public class ScreenshotTest {
 
@@ -67,6 +77,9 @@ public class ScreenshotTest {
 
         new ImportDatabaseAsyncTask(activity).execute(Uri.parse(uriString));
 
+        guaranteeCheck(R.string.show_hidden_li_entries, true);
+        guaranteeCheck(R.string.night_mode, false);
+
         sleep(3000);
     }
 
@@ -93,6 +106,27 @@ public class ScreenshotTest {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void guaranteeCheck(int stringId, boolean checked){
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        try {
+            // The checkbox of menu item is hidden pretty deep
+            ViewInteraction v = onView(allOf(Matchers.<View>instanceOf(CheckBox.class), hasSibling(withChild(withText(stringId)))));
+
+            if(checked) {
+                v.check(matches(isChecked()));
+            }
+            else {
+                v.check(matches(not(isChecked())));
+            }
+
+            Espresso.pressBack();
+
+        } catch (AssertionFailedError e){
+            onView(withText(stringId)).perform(click());
         }
     }
 }
