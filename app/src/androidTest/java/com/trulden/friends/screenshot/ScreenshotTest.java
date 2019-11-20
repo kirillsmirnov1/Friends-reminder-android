423813ff38c420be5ca32e9f4de526b32179fee1
kirillsmirnov1 <thegaldren@gmail.com>
2019-11-20 16:47:58 +0300

    make screenshot of journal

--- app/src/androidTest/java/com/trulden/friends/screenshot/ScreenshotTest.java
+++ app/src/androidTest/java/com/trulden/friends/screenshot/ScreenshotTest.java
@@ -1,5 +1,6 @@
 package com.trulden.friends.screenshot;
 
+import com.trulden.friends.R;
 import com.trulden.friends.activity.AbstractMATest;
 
 import org.junit.Before;
@@ -9,6 +10,10 @@ import org.junit.Test;
 import tools.fastlane.screengrab.Screengrab;
 import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
 import tools.fastlane.screengrab.locale.LocaleTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class ScreenshotTest extends AbstractMATest {
 
@@ -23,6 +28,11 @@ public class ScreenshotTest extends AbstractMATest {
     @Test
     public void captureScreen(){
         sleep(500);
-        Screengrab.screenshot("main_screen");
+        Screengrab.screenshot("trackers");
+
+        onView(withId(R.id.mbn_interactions)).perform(click());
+        sleep(500);
+
+        Screengrab.screenshot("journal");
     }
 }
